cf4fa4ecbf6b614aff33afe0c29f28c552cf5968
Michael Stegeman <michael@stegeman.me>
2019-10-04 09:22:55 -0800

    Updates for Gateway 0.10.

--- package.sh
+++ package.sh
@@ -5,18 +5,19 @@ set -e
 version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 
 # Clean up from previous releases
-rm -rf *.tgz package
-rm -f SHA256SUMS
+rm -rf *.tgz package SHA256SUMS
+
+# Prep new package
+mkdir package
 
 # Put package together
-mkdir package
-cp -r pkg LICENSE package.json *.py requirements.txt setup.cfg package/
+cp -r pkg LICENSE manifest.json package.json *.py README.md requirements.txt setup.cfg package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
 # Generate checksums
 cd package
-sha256sum *.py pkg/*.py LICENSE requirements.txt setup.cfg > SHA256SUMS
+find . -type f -exec sha256sum {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
